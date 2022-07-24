18454a85181aa4d9e8e8af5e68fac49b434f2354
bugsounet <bugsounet@bugsounet.fr>
2022-07-24 18:16:28 +0200

    add some Greek translations

--- EXT-Raspotify.js
+++ EXT-Raspotify.js
@@ -33,7 +33,8 @@ Module.register("EXT-Raspotify", {
       es: "translations/es.json",
       nl: "translations/nl.json",
       pt: "translations/pt.json",
-      ko: "translations/ko.json"
+      ko: "translations/ko.json",
+      el: "translations/el.json"
     }
   },
 
